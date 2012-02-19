435a85a68533c4595526250b62943c19564c77f4
Coolkid <s196774065@gmail.com>
2012-02-19 23:40:47 +0800

    123

--- Interface/Addons/s_Core/layout/guild.lua
+++ Interface/Addons/s_Core/layout/guild.lua
@@ -17,7 +17,7 @@ local ceil			= math.ceil
 local tthead, ttsubh, ttoff = {r=0.4, g=0.78, b=1}, {r=0.75, g=0.9, b=1}, {r=.3,g=1,b=.3}
 local activezone, inactivezone = {r=0.3, g=1.0, b=0.3}, {r=0.65, g=0.65, b=0.65}
 local displayString = join("", GUILD, ": %d|r")
-local noGuildString = join("", "","No Guild")
+local noGuildString = join("", "",L["没有工会"])
 local guildInfoString = "%s [%d]"
 local guildInfoString2 = join("", GUILD, ": %d/%d")
 local guildMotDString = "%s |cffaaaaaa- |cffffffff%s"
@@ -108,7 +108,7 @@ local function Update(self, event, ...)
 		Guild:SetMinMaxValues(0, total)
 		Guild:SetValue(online)
 	else
-		Guild:SetText(L["没有工会"])
+		Guild.Text:SetText(L["没有工会"])
 	end
 	self:SetAllPoints(Guild)
 end
