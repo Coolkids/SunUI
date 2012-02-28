59b12d051ba038c29cfa75782f3ee76685b8d972
Coolkid <s196774065@gmail.com>
2012-02-28 16:38:49 +0800

    123

--- Interface/Addons/s_Core/mini/Settings.lua
+++ Interface/Addons/s_Core/mini/Settings.lua
@@ -98,28 +98,7 @@ end
 
 SLASH_WATCHFRAMELOCK1 = "/wf"
 SlashCmdList["WATCHFRAMELOCK"] = WATCHFRAMELOCK
---任务追踪字体
-local WFT = _G["WatchFrameTitle"]
-   WFT:SetFont(DB.Font, 17*MiniDB["FontScale"]*S.Scale(1), "THINOUTLINE")
-   WFT:SetShadowOffset(0, 0)
-   WFT:SetShadowColor(0, 0, 0, 1)
-   hooksecurefunc("WatchFrame_SetLine", function(line)
-      line.text:SetFont(DB.Font, 14*MiniDB["FontScale"]*S.Scale(1), "THINOUTLINE")
-      line.text:SetShadowOffset(0, 0)
-      line.text:SetShadowColor(0, 0, 0, 1)
-      if line.dash then
-         line.dash:SetFont(DB.Font, 14*MiniDB["FontScale"]*S.Scale(1), "THINOUTLINE")
-         line.text:SetShadowOffset(0, 0)
-         line.text:SetShadowColor(0, 0, 0, 1)
-      end
-   end)
---任务按钮
-S.Reskin(WatchFrameCollapseExpandButton)
-WatchFrameCollapseExpandButton.text = WatchFrameCollapseExpandButton:CreateFontString(nil, 'OVERLAY')
-WatchFrameCollapseExpandButton.text:SetFont(DB.Font, 14*MiniDB["FontScale"]*S.Scale(1))
-WatchFrameCollapseExpandButton.text:SetText("+")
-WatchFrameCollapseExpandButton.text:SetPoint("CENTER", 1, 2)
-WatchFrameCollapseExpandButton.text:SetTextColor(1.0, 0.82, 0)
+
 -- simple spec and equipment switching
 SlashCmdList["SPEC"] = function() 
 	if GetActiveTalentGroup()==1 then SetActiveTalentGroup(2) elseif GetActiveTalentGroup()==2 then SetActiveTalentGroup(1) end
