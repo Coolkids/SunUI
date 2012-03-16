1396a1a7beb634866c28fe41a81a118bf80b8fb0
Coolkid <s196774065@gmail.com>
2012-03-16 12:08:33 +0800

    sh.lua

--- Interface/Addons/!SunUI/mini/Class/sh.lua
+++ Interface/Addons/!SunUI/mini/Class/sh.lua
@@ -1,4 +1,5 @@
 local S, _, _, DB = unpack(select(2, ...))
+local class = select(2, UnitClass("player"))
 local Frame = CreateFrame("Frame")
 	  Frame:Width(48)
       Frame:Height(48)  
@@ -9,71 +10,71 @@ local Frame = CreateFrame("Frame")
 	  
 	  Frame:CreateShadow("Background")
 	  Frame:Hide()
-local function UpdateCDFrame(index, name, icon, start, duration)
-	if Frame.Cooldown then
-		Frame.Cooldown:SetReverse(false)
-		CooldownFrame_SetTimer(Frame.Cooldown, start, duration, 1)
+local spellIDs = {
+	["PRIEST"] = 32379,
+	["HUNTER"] = 53351,
+	["MAGE"] = 12051,
+	["WARLOCK"] = nil,
+	["PALADIN"] = nil,
+	["ROGUE"] = nil,
+	["DRUID"] = nil,
+	["SHAMAN"] = nil,
+	["WARRIOR"] = nil,
+	["DEATHKNIGHT"] = nil,
+} 
+local function UpdateCDFrame() 
+		local start, duration = GetSpellCooldown(spellIDs[class])
+		if start and duration > 1.5 then
+			if Frame.Cooldown then
+				Frame.Cooldown:SetReverse(false)
+				CooldownFrame_SetTimer(Frame.Cooldown, start, duration, 1)
+			end
+		end
+		if event == "SPELL_UPDATE_COOLDOWN" then
+			CooldownFrame_SetTimer(Frame.Cooldown, start, 0, 1)
+		end
+end
+local function  UpdateMakeIcon()
+	local Icon = select(3, GetSpellInfo(spellIDs[class]))
+	Frame.Icon = Frame:CreateTexture(nil, "ARTWORK") 
+	Frame.Icon:SetTexture(Icon) 
+	Frame.Icon:SetAllPoints(Frame)
+	Frame.Icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+end
+
+Frame:SetScript("OnEvent", function(self, event)
+	if class == "PRIEST" and UnitLevel("player") == 85 then 
+		if ( UnitCanAttack("player", "target") and not UnitIsDead("target") and ( UnitHealth("target")/UnitHealthMax("target") < 1.25 ) ) then
+			UpdateMakeIcon()
+			self:Show()
+			UpdateCDFrame()
+		else self:Hide()
+		end
 	end
-end
-local function  UpdateMakeIcon(Icon)
-Frame.Icon = Frame:CreateTexture(nil, "ARTWORK") 
-Frame.Icon:Width(48)
-Frame.Icon:Height(48)
-Frame.Icon:SetTexture(Icon) 
-Frame.Icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
-Frame.Icon:Point("CENTER", Frame, "CENTER", 0, 0)
-end
+   if class == "HUNTER" and UnitLevel("player") == 85 then 
+		if ( UnitCanAttack("player", "target") and not UnitIsDead("target") and ( UnitHealth("target")/UnitHealthMax("target") < 0.2 ) ) then
+			UpdateMakeIcon()
+			self:Show()
+			UpdateCDFrame()
+		else self:Hide()
+		end
+   end
+   if class == "MAGE" and UnitLevel("player") == 85 then 
+		if ( UnitPower("player")/UnitPowerMax("player") < 0.4 ) then
+			UpdateMakeIcon()
+			self:Show()
+			UpdateCDFrame()
+		else self:Hide()
+		end
+	end
+end)	
+
 Frame:RegisterEvent("UNIT_HEALTH")
 Frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 Frame:RegisterEvent("UNIT_POWER")
+Frame:RegisterEvent("SPELL_UPDATE_COOLDOWN")
 
-Frame:SetScript("OnEvent", function(self, event)
-	if DB.MyClass == "PRIEST" and UnitLevel("player") == 85 then 
-		if event == "UNIT_HEALTH" or event == "PLAYER_TARGET_CHANGED"  then 
-			if ( UnitCanAttack("player", "target") and not UnitIsDead("target") and ( UnitHealth("target")/UnitHealthMax("target") < 0.25 ) ) then
-			local _, _, Icon = select(1, GetSpellInfo(32379))
-			 UpdateMakeIcon(Icon)
-			self:Show()
-				if GetSpellCooldown(32379) and select(2, GetSpellCooldown(32379)) > 1.5 then
-					local start, duration = GetSpellCooldown(32379)
-					UpdateCDFrame(KEY, name, icon, start, duration)
-				end 
-			else self:Hide()
-			end
-		end
-    end
-   if DB.MyClass == "HUNTER" and UnitLevel("player") == 85 then 
-		if event == "UNIT_HEALTH" or event == "PLAYER_TARGET_CHANGED"  then 
-			if ( UnitCanAttack("player", "target") and not UnitIsDead("target") and ( UnitHealth("target")/UnitHealthMax("target") < 0.2 ) ) then
-			local _, _, Icon = select(1, GetSpellInfo(53351))
-			UpdateMakeIcon(Icon)
-			self:Show()
-				if GetSpellCooldown(53351) and select(2, GetSpellCooldown(53351)) > 1.5 then
-					local start, duration = GetSpellCooldown(53351)
-					UpdateCDFrame(KEY, name, icon, start, duration)
-				end 
-			else self:Hide()
-			end
-		end
-   end
-   if DB.MyClass == "MAGE" and UnitLevel("player") == 85 then 
-		if event == "UNIT_POWER" then 
-			if ( UnitPower("player")/UnitPowerMax("player") < 0.4 ) then
-			local _, _, Icon = select(1, GetSpellInfo(12051))
-			UpdateMakeIcon(Icon)
-			self:Show()
-				if GetSpellCooldown(12051) and select(2, GetSpellCooldown(12051)) > 1.5 then
-					local start, duration = GetSpellCooldown(12051)
-					UpdateCDFrame(KEY, name, icon, start, duration)
-				end 
-			else self:Hide()
-			end
-		end
-   end
-end)	
-
-
-if DB.MyClass=="PRIEST" then
+if class=="PRIEST" then
 	local sp=CreateFrame("Frame")
 	sp:SetScript("OnEvent",function(self)
 		if GetShapeshiftForm() == 1 then
