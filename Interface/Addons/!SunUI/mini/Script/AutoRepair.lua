44fe2ceec4bf0da825bc36d170b799331f2ecdf3
Coolkid <s196774065@gmail.com>
2012-05-01 02:35:21 +0800

    删除多余材质 微调

--- Interface/Addons/!SunUI/mini/Script/AutoRepair.lua
+++ Interface/Addons/!SunUI/mini/Script/AutoRepair.lua
@@ -23,7 +23,6 @@ function Module:OnInitialize()
 					end
 					if guildMoney > cost and CanGuildBankRepair() then
 						RepairAllItems(1)
-						print(cost)
 						DEFAULT_CHAT_FRAME:AddMessage("|cffffff00您修理装备花费了公会：|r"..format(GOLD_AMOUNT_TEXTURE, g, 0, 0).." "..format(SILVER_AMOUNT_TEXTURE, s, 0, 0).." "..format(COPPER_AMOUNT_TEXTURE, c, 0, 0),255,255,255)
 						return
 					end
