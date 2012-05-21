99fcf5be457f9ae6e28a13328c69fa3ef8571ae9
Coolkid <s196774065@gmail.com>
2012-05-21 23:04:19 +0800

    自带修理

--- Interface/Addons/SunUI_Script/mini/Script/AutoRepair.lua
+++ Interface/Addons/SunUI_Script/mini/Script/AutoRepair.lua
@@ -16,15 +16,12 @@ function Module:OnInitialize()
 				local g = math.floor(cost/10000)
 				if IsInGuild() then
 					local guildMoney = GetGuildBankWithdrawMoney()
-					if GetGuildBankMoney() ~= 0 then
-						if guildMoney > GetGuildBankMoney() then
-							guildMoney = GetGuildBankMoney()
-						end
-					end
 					if guildMoney > cost and CanGuildBankRepair() then
 						RepairAllItems(1)
 						DEFAULT_CHAT_FRAME:AddMessage("|cffffff00您修理装备花费了公会：|r"..format(GOLD_AMOUNT_TEXTURE, g, 0, 0).." "..format(SILVER_AMOUNT_TEXTURE, s, 0, 0).." "..format(COPPER_AMOUNT_TEXTURE, c, 0, 0),255,255,255)
 						return
+					else
+						RepairAllItems()
 					end
 				end
 				if possible and not CanGuildBankRepair() then
