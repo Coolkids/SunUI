76377b1fb2b8bf32272cb3a3d374d2806167a3d7
Coolkid <s196774065@gmail.com>
2012-12-04 20:21:01 +0800

    block

--- Interface/Addons/!BaudErrorFrame/BaudErrorFrame.lua
+++ Interface/Addons/!BaudErrorFrame/BaudErrorFrame.lua
@@ -6,7 +6,7 @@ local EnableSound = false
 
 function BaudErrorFrame_OnLoad(self)
 	self:RegisterEvent("VARIABLES_LOADED")
-	self:RegisterEvent("ADDON_ACTION_BLOCKED")
+	--self:RegisterEvent("ADDON_ACTION_BLOCKED")
 	self:RegisterEvent("MACRO_ACTION_BLOCKED")
 	self:RegisterEvent("ADDON_ACTION_FORBIDDEN")
 	UIParent:UnregisterEvent("ADDON_ACTION_FORBIDDEN")
