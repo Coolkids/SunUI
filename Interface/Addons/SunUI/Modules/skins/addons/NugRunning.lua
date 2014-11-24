14a9d2be3ca59686e2de44f592cd034ee97aca33
coolkid <s196774065@gmail.com>
2014-11-24 19:29:25 +0800

    NugRunning美化调整

--- Interface/Addons/SunUI/Modules/skins/addons/NugRunning.lua
+++ Interface/Addons/SunUI/Modules/skins/addons/NugRunning.lua
@@ -3,10 +3,10 @@ local A = S:GetModule("Skins")
 
 --(\d+)(\s*,.+name\s*=\s*)\"[^"]+\"替换\1\2GetSpellInfo\(\1\)
 local function Skin()
-	NugRunningConfig.nameFont = { font = S["media"].font, S["media"].fontsize, alpha = 0.5 }
-	NugRunningConfig.timeFont = { font = S["media"].font, S["media"].fontsize, alpha = 1 }
-	NugRunningConfig.stackFont = { font = S["media"].font, S["media"].fontsize }
-	NugRunningConfig.dotpowerFont = { font = S["media"].font, S["media"].fontsize-1,  alpha = 1  }
+	NugRunningConfig.nameFont = { font = S["media"].font, size = S["media"].fontsize, alpha = 0.5 }
+	NugRunningConfig.timeFont = { font = S["media"].font, size = S["media"].fontsize, alpha = 1 }
+	NugRunningConfig.stackFont = { font = S["media"].font, size = S["media"].fontsize }
+	NugRunningConfig.dotpowerFont = { font = S["media"].font, size = S["media"].fontsize-1,  alpha = 1  }
 	
 	NugRunningConfig.anchors = {
 		main = {
@@ -20,189 +20,7 @@ local function Skin()
 		},
 	}
 	-- Replace bar creation function
-	ConstructTimerBar = function(width, height)
-		local f = CreateFrame("Frame",nil,UIParent)
-		f.prototype = "TimerBar"
-
-		f:SetWidth(width-height)
-		f:SetHeight(height+4)
-		
-		
-		local ic = CreateFrame("Frame",nil,f)
-		ic:SetPoint("BOTTOMRIGHT",f,"BOTTOMLEFT", -10, 0)
-		ic:SetWidth(height+2)
-		ic:SetHeight(height+2)
-		ic:CreateShadow("Background")
-		
-		local ict = ic:CreateTexture(nil,"ARTWORK",0)
-		ict:SetTexCoord(.07, .93, .07, .93)
-		ict:SetAllPoints(ic)
-		f.icon = ict
-		
-		f.stacktext = ic:CreateFontString(nil, "OVERLAY");
-		f.stacktext:SetFont(S["media"].font, S["media"].fontsize, "OUTLINE")
-		f.stacktext:SetJustifyH("RIGHT")
-		f.stacktext:SetVertexColor(1,1,1)
-		f.stacktext:SetPoint("LEFT", ic, "RIGHT",-9,-3)
-		
-		f.bar = CreateFrame("StatusBar",nil,f)
-		f.bar:CreateShadow("Background")
-		
-		f.bar:SetFrameStrata("MEDIUM")
-		f.bar:SetStatusBarTexture(S["media"].normal)
-		f.bar:GetStatusBarTexture():SetDrawLayer("ARTWORK")
-		f.bar:SetHeight(height/2)
-		f.bar:SetWidth(width - height )
-		f.bar:SetPoint("BOTTOMRIGHT",f,"BOTTOMRIGHT",0,0)
-		A:CreateMark(f.bar)
-		
-		f.bar.bg = f.bar:CreateTexture(nil, "BORDER")
-		f.bar.bg:SetAllPoints(f.bar)
-		--f.bar.bg:SetTexture(tex)
-		
-		f.timeText = f.bar:CreateFontString(nil, "OVERLAY", 2);
-		f.timeText:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
-		f.timeText:SetJustifyH("LEFT")
-		f.timeText:SetVertexColor(1,1,1)
-		f.timeText:SetPoint("LEFT", f.bar, "LEFT",0,height/2)
-			
-		f.timeText.SetFormattedText = SetTimeText
-		
-		f.spellText = f.bar:CreateFontString(nil, "OVERLAY", 2);
-		f.spellText:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
-		f.spellText:SetWidth(f.bar:GetWidth()*0.7)
-		f.spellText:SetHeight(height/2+1)
-		f.spellText:SetJustifyH("RIGHT")
-		f.spellText:SetPoint("RIGHT", f.bar, "RIGHT",0,height/2)
-		f.spellText.SetName = SpellTextUpdate
-		
-		local overlay = f.bar:CreateTexture(nil, "ARTWORK", nil, 3)
-		overlay:SetTexture("Interface\\Tooltips\\UI-Tooltip-Background")
-		overlay:SetVertexColor(0,0,0, 0.2)
-		overlay:Hide()
-		f.overlay = overlay
-		
-		f.SetColor = TimerBarSetColor
-		
-		local powertext = f.bar:CreateFontString()
-	    powertext:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
-		powertext:SetPoint("BOTTOMLEFT", f.bar, "BOTTOMRIGHT",2,0)
-		powertext:SetShadowColor(0, 0, 0)
-		powertext:SetShadowOffset(1, -1)
-		
-		local sbg = f.bar:CreateTexture(nil, "ARTWORK", nil, 5)
-		sbg:SetTexture("Interface\\Tooltips\\UI-Tooltip-Background")
-		sbg:SetVertexColor(0,0,0, 0)
-		sbg:SetAllPoints(powertext)
-		powertext.bg = sbg
-		f.status = powertext
-		
-		
-		local at = ic:CreateTexture(nil,"OVERLAY")
-		at:SetTexture([[Interface\SpellActivationOverlay\IconAlert]])
-		at:SetTexCoord(0.00781250,0.50781250,0.27734375,0.52734375)
-		at:SetWidth(height*1.8)
-		at:SetHeight(height*1.8)
-		at:SetPoint("CENTER",f.icon,"CENTER",0,0)
-		at:SetAlpha(0)
-		
-		local sag = at:CreateAnimationGroup()
-		local sa1 = sag:CreateAnimation("Alpha")
-		sa1:SetChange(1)
-		sa1:SetDuration(0.3)
-		sa1:SetOrder(1)
-		local sa2 = sag:CreateAnimation("Alpha")
-		sa2:SetChange(-1)
-		sa2:SetDuration(0.5)
-		sa2:SetSmoothing("OUT")
-		sa2:SetOrder(2)
-		
-		f.shine = sag
-		f.shine.tex = at
-		
-		local aag = f:CreateAnimationGroup()
-		local aa1 = aag:CreateAnimation("Scale")
-		aa1:SetOrigin("BOTTOM",0,0)
-		aa1:SetScale(1,0.1)
-		aa1:SetDuration(0)
-		aa1:SetOrder(1)
-		local aa2 = aag:CreateAnimation("Scale")
-		aa2:SetOrigin("BOTTOM",0,0)
-		aa2:SetScale(1,10)
-		aa2:SetDuration(0.15)
-		aa2:SetOrder(2)
-		
-		local glow = f:CreateAnimationGroup()
-		local ga1 = glow:CreateAnimation("Alpha")
-		ga1:SetChange(-0.5)
-		ga1:SetDuration(0.25)
-		ga1:SetOrder(1)
-		glow:SetLooping("BOUNCE")
-		f.glow = glow
-		
-		f.animIn = aag
-			 
-		local m = CreateFrame("Frame",nil,self)
-		m:SetParent(f)
-		m:SetWidth(16)
-		m:SetHeight(f:GetHeight()/2)
-		m:SetFrameLevel(4)
-		m:SetAlpha(1)
-		
-		local texture = m:CreateTexture(nil, "OVERLAY")
-		texture:SetTexture("Interface\\AddOns\\SunUI\\media\\mark")
-		texture:SetVertexColor(0,0,0,1)
-		texture:SetAllPoints(m)
-		m.texture = texture
-		
-		local spark = m:CreateTexture(nil, "OVERLAY")
-		spark:SetTexture("Interface\\CastingBar\\UI-CastingBar-Spark")
-		spark:SetAlpha(0)
-		spark:SetWidth(20)
-		spark:SetHeight(m:GetWidth()*4)
-		spark:SetPoint("CENTER",m)
-		spark:SetBlendMode('ADD')
-		spark.mark = m
-		spark.CatchUp = function(self)
-			local markpoint = 
-			self:SetPoint(self.mark:GetPoint())
-		end
-		m.spark = spark
-		
-		local ag = spark:CreateAnimationGroup()
-		local a1 = ag:CreateAnimation("Alpha")
-		a1:SetChange(1)
-		a1:SetDuration(0.2)
-		a1:SetOrder(1)
-		local a2 = ag:CreateAnimation("Alpha")
-		a2:SetChange(-1)
-		a2:SetDuration(0.4)
-		a2:SetOrder(2)
-		
-		m.shine = ag
-
-		f.mark = m
-
-		return f
-	end
-	NugRunning.ConstructTimerBar = ConstructTimerBar
-
-	function NugRunning.TimerBar.SetPowerStatus(self, status, powerdiff)
-		if status == "HIGH" then
-			self.status:SetTextColor(.1,1,.1)
-			self.status:SetText("+"..powerdiff)
-			self.status:Show()
-			self.status.bg:Hide()
-		elseif status == "LOW" then
-			self.status:SetTextColor(1,.1,.1)
-			self.status:SetText(powerdiff)
-			self.status:Show()
-			self.status.bg:Hide()
-		else
-			self.status:Hide()
-			self.status.bg:Hide()
-		end
-	end
+	
 	function NugRunning:DoNameplates()
 
 		local next = next
@@ -388,6 +206,65 @@ local function Skin()
 			end
 		end
 	end
+	
+	local NugRunning = NugRunning
+	local TimerBar = NugRunning and NugRunning.TimerBar
+
+	-- Creates timerbar
+	local _ConstructTimerBar = NugRunning.ConstructTimerBar
+	function NugRunning.ConstructTimerBar(w, h)
+		local f = _ConstructTimerBar(w, h)
+
+		f:SetBackdrop(nil)
+
+		local ic = f.icon:GetParent()
+		ic:CreateShadow()
+
+		f.bar:CreateShadow()
+
+		f.bar:SetStatusBarTexture(S["media"].normal)
+		f.bar:GetStatusBarTexture():SetDrawLayer("ARTWORK")
+		f.bar.bg:SetTexture(S["media"].normal)
+		f.bar.bg:SetAlpha(0.5)
+		A:CreateMark(f.bar)
+
+		f.timeText:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
+		f.timeText:SetShadowOffset(S.mult, -S.mult)
+		f.spellText:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
+		f.spellText:SetShadowOffset(S.mult, -S.mult)
+		f.stacktext:SetFont(S["media"].font, S["media"].fontsize, "THINOUTLINE")
+		f.stacktext:SetShadowOffset(S.mult, -S.mult)
+
+		TimerBar.Resize(f, w, h)
+
+		return f
+	end
+
+	-- Resizes timerbar
+	local _Resize = TimerBar.VScale
+	function TimerBar.Resize(f, w, h)
+		_Resize(f, w, h)
+
+		local ic = f.icon:GetParent()
+		ic:ClearAllPoints()
+		ic:SetPoint("TOPLEFT", f, 1, -1)
+		ic:SetPoint("BOTTOMLEFT", f, 1, 0)
+		
+		f.bar:ClearAllPoints()
+		f.bar:SetPoint("TOPRIGHT", f, -1, -1)
+		f.bar:SetPoint("BOTTOMRIGHT", f, -1, 0)
+		f.bar:SetPoint("LEFT", ic, "RIGHT", 5, 0)
+		
+		f.timeText:SetJustifyH("RIGHT")
+		f.timeText:ClearAllPoints()
+		f.timeText:SetPoint("RIGHT", 1, 0)
+
+		f.spellText:SetJustifyH("LEFT")
+		f.spellText:ClearAllPoints()
+		f.spellText:SetPoint("LEFT", 2, 0)
+		f.spellText:SetWidth(f.bar:GetWidth() - 10)
+	end
+	
 end
 
 A:RegisterSkin("NugRunning", Skin)
