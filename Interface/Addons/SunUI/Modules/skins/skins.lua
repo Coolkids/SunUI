b381a2ae44d016fed79587cdba34123f4909cc4d
yclwll <yclwll@gmail.com>
2014-12-07 12:28:13 +0800

    UP

--- Interface/Addons/SunUI/Modules/skins/skins.lua
+++ Interface/Addons/SunUI/Modules/skins/skins.lua
@@ -656,6 +656,39 @@ function A:ReskinNavBar(f)
 	overflowButton:HookScript("OnLeave", clearArrow)
 end
 
+function A:ReskinGarrisonPortrait(portrait)
+	local level = portrait.Level
+	local cover = portrait.PortraitRingCover
+
+	portrait.PortraitRing:Hide()
+	portrait.PortraitRingQuality:SetTexture("")
+
+	portrait.LevelBorder:SetTexture(0, 0, 0, .5)
+	portrait.LevelBorder:SetSize(44, 11)
+	portrait.LevelBorder:ClearAllPoints()
+	portrait.LevelBorder:SetPoint("BOTTOM", 0, 12)
+
+	level:ClearAllPoints()
+	level:SetPoint("BOTTOM", portrait, 0, 12)
+
+	local squareBG = CreateFrame("Frame", nil, portrait)
+	squareBG:SetFrameLevel(portrait:GetFrameLevel()-1)
+	squareBG:SetPoint("TOPLEFT", 3, -3)
+	squareBG:SetPoint("BOTTOMRIGHT", -3, 11)
+	A:CreateBD(squareBG, 1)
+	portrait.squareBG = squareBG
+
+	if cover then
+		cover:SetTexture(0, 0, 0)
+		cover:SetAllPoints(squareBG)
+	end
+end
+
+function A:ReskinIcon(icon)
+	icon:SetTexCoord(.08, .92, .08, .92)
+	A:CreateBG(icon)
+end
+
 function A:CreateGradient(f)
 	local tex = f:CreateTexture(nil, "BORDER")
 	tex:SetPoint("TOPLEFT", 1, -1)
